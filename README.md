f34a4a5b1a017ebdb3d650f0d8156f40aeee0291
wan <wan@tagbangers.co.jp>
2021-09-28 23:33:53 +0900

    supplement

---
+++ README.md
@@ -0,0 +1,7 @@
+For ***VSCode***, make sure these extensions in place:
+- Extension Pack For Java
+- Lombok Annotations Support for VS Code
+- Spring Boot Extension Pack
+
+For ***Intellij Idea***, make sure this plugin in place:
+- EditStarters
--- src/main/java/com/example/myproject/MyprojectApplication.java
+++ src/main/java/com/example/myproject/MyprojectApplication.java
@@ -3,7 +3,7 @@ package com.example.myproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  // 起動クラスを表す、コンフィグクラスでもある、プロジェクトソースのrootに置くこと
 public class MyprojectApplication {
 
 	public static void main(String[] args) {
